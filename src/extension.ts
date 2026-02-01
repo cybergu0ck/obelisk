703ec6623ab6cc0f19d05baef7a3dce579b55d81
cybergu0ck <agneshraok.apps@gmail.com>
2026-02-01 13:53:47 +0530

    Feat : Add activate code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { chiselContent } from "./formatter";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "obelisk.chisel",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("No file is currently open");
+        return;
+      }
+      const document = editor.document;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "obelisk" is now active!');
+      if (document.languageId === "markdown") {
+        const content = document.getText();
+        const chiseledContent = chiselContent(content);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('obelisk.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from obelisk!');
-	});
+        const edit = new vscode.WorkspaceEdit();
+        const fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(document.getText().length),
+        );
+        edit.replace(document.uri, fullRange, chiseledContent);
+        await vscode.workspace.applyEdit(edit);
+        vscode.window.showInformationMessage(
+          "Current file is chiseled succesfully",
+        );
+      } else {
+        vscode.window.showInformationMessage(
+          "Current file is not a markdown file",
+        );
+      }
+    },
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
